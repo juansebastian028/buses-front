eb501a30834d6f2a8cda6ba86bee21d971738faf
Juan Sebastian <juansebastianvargasposada@gmail.com>
2022-05-14 15:26:25 -0500

    14/05/2022

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import { AppRouter } from "./routes/AppRouter";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import { persistor, store } from "./store/store";
-import "antd/dist/antd.css";
 
 function App() {
   return (
